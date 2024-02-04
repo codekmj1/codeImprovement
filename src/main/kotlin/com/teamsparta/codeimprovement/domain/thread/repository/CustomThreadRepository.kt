1cb8baf039db7127a4ae66373478806838fb5b15
codekmj1 <codekmj@gmail.com>
2024-02-04 11:31:27 +0900

    fix: CustomThreadRepository Pagable인자  추가, 반환타입을 Page로 변경

--- src/main/kotlin/com/teamsparta/codeimprovement/domain/thread/repository/CustomThreadRepository.kt
+++ src/main/kotlin/com/teamsparta/codeimprovement/domain/thread/repository/CustomThreadRepository.kt
@@ -2,8 +2,10 @@ package com.teamsparta.codeimprovement.domain.thread.repository
 
 import com.teamsparta.codeimprovement.domain.thread.model.Channel
 import com.teamsparta.codeimprovement.domain.thread.model.Thread
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 
 
 interface CustomThreadRepository {
-    fun findAllByChannelAndMessage(channel: Channel, keyword: String): List<Thread>
+    fun findAllByChannelAndMessage(channel: Channel, keyword: String, pageable: Pageable): Page<Thread>
 }
